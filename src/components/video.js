f6d0146ebfc92b89194255e1636d71c86b441f83
deanrivers <deanriversdeveloper@gmail.com>
2020-04-06 16:46:43 -0400

    Updated hamburger menu

--- src/components/video.js
+++ src/components/video.js
@@ -5,19 +5,18 @@ import FadeIn from 'react-fade-in'
 let Video = () =>{
     
         return(
-            <FadeIn>
-                <div id="video-section-container">
-                    <div id="video-header-container">
-                        <h1>REAL FREESTYLER<span className="orange-text">.</span></h1>
-                        <h1><span className="orange-text">REAL BALLER</span>.</h1>
-                    </div>
-
-                    <div id="iframe-container">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/JbMm_bIrQGA" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    </div>
-                    <Gallery/>                
+            <div id="video-section-container">
+                <div id="video-header-container">
+                    <h1>REAL FREESTYLER<span className="orange-text">.</span></h1>
+                    <h1><span className="orange-text">REAL BALLER</span>.</h1>
                 </div>
-            </FadeIn>
+
+                <div id="iframe-container">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/JbMm_bIrQGA" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
+                <Gallery/>                
+            </div>
+
         )
     
         
