40f2f3bfd94313ae64e614ca003300d096795cdd
deanrivers <deanriversdeveloper@gmail.com>
2020-04-16 23:14:40 -0400

    Updated carousel

--- src/components/brands.js
+++ src/components/brands.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {Carousel} from 'react-responsive-carousel'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 //images
 import adidasImage from '../assets/Brands/adidas.png'
 import atletiImage from '../assets/Brands/atleti.png'
@@ -15,19 +18,40 @@ import tmobileImage from '../assets/Brands/tmobile.png'
 import univisionImage from '../assets/Brands/univision.png'
 import usaImage from '../assets/Brands/usa.png'
 
+
+{/* <Carousel
+autoPlay={true}
+infiniteLoop={true}
+showThumbs={false}
+showArrows={false}
+swipeable={true}
+interval={2000}
+> */}
+
 let Brands = () =>{
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed:2000,
+        dots:false,
+        draggable:false,
+        fade:false,
+        infinite:true,
+        swipe: true
+        
+      };
     return(
+
         <div id="brand-carousel">
-            {/* <h1>Brands I've worked with.</h1> */}
-            <Carousel
-                autoPlay={true}
-                infiniteLoop={true}
-                
-                showThumbs={false}
-                showArrows={false}
-                
-                
-                >
+            <h1>Brands I've worked with.</h1>
+            <br/>
+            <br/>
+            <Slider {...settings}
+            >
                 <img src={adidasImage}/>
                 <img src={atletiImage}/>
                 <img src={audiImage}/>
@@ -40,7 +64,7 @@ let Brands = () =>{
                 <img src={tmobileImage}/>
                 <img src={univisionImage}/>
                 <img src={usaImage}/>
-            </Carousel>
+            </Slider>
             
         </div>
     )
--- src/components/social-counter.js
+++ src/components/social-counter.js
@@ -7,11 +7,14 @@ import CountUp from 'react-countup'
 
 let SocialCounter = () =>{
     return([
-        // <div id="counter-header"><h1>Socials</h1></div>,
-        <div id="counter-container">
+        
+        <div id="social-container">
+            <div id="counter-header"><h1>Socials.</h1></div>
+            <div id="counter-container">
+                <div className="counter-children"><img src={igWhite} alt="ig-white"/><CountUp end={7.5} duration={10} suffix="k followers" decimals={1}/></div>
+                <div className="counter-children"><img src={tiktokWhite} alt="tiktok-white"/><CountUp end={249.6} duration={10} suffix="k followers" decimals={1}/></div>
             
-            <div className="counter-children"><img src={igWhite} alt="ig-white"/><CountUp end={7.5} duration={10} suffix="k followers" decimals={1}/></div>
-            <div className="counter-children"><img src={tiktokWhite} alt="tiktok-white"/><CountUp end={249.6} duration={10} suffix="k followers" decimals={1}/></div>
+            </div>
             {/* <div className="counter-children"><img src={ytWhite} alt="yt-white"/><CountUp end={100} duration={10} suffix="k followers" decimals={1}/></div> */}
         </div>
     ])
